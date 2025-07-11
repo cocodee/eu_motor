5c337c08772d9b62414efdf728902c2390deddeb
dikeke <dikeke@qiniu.com>
2025-07-11 13:45:32 +0800

    test

--- src/eu_motor.cpp
+++ src/eu_motor.cpp
@@ -296,7 +296,6 @@ void MotorFeedbackManager::canRecvCallback(int devIndex,const harmonic_CanMsg* f
     // TPDO1: 0x180, TPDO2: 0x280, TPDO3: 0x380, TPDO4: 0x480
     huint32 cob_id_base = frame->cob_id & 0xFFFFFF80;
     huint8 node_id = frame->cob_id & 0x0000007F;
-    std::cout << "Node ID: " << std::dec << node_id << std::endl;
     if ((cob_id_base >= 0x180 && cob_id_base <= 0x480) && frame->len == 8) {
         //std::cout << "Received TPDO frame" << std::hex << frame->data << std::endl;
         std::lock_guard<std::mutex> lock(mutex_);
