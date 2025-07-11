2dd93d5d6bbc0e3ff0073037a69b6ced77f92533
dikeke <dikeke@qiniu.com>
2025-07-11 12:39:05 +0800

    test

--- example/test_pdo.cpp
+++ example/test_pdo.cpp
@@ -50,8 +50,8 @@ int main()
     }
     std::this_thread::sleep_for(std::chrono::milliseconds(100));
 
-    // harmonic_setSendDataCallBack(sendCallback);
-    // harmonic_setReceiveDataCallBack(receiveCallback);
+    harmonic_setSendDataCallBack(sendCallback);
+    harmonic_setReceiveDataCallBack(receiveCallback);
 
     int id = 21;
     int autoReportMs = 100;
