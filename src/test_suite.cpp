352b018b791d2539316adf5725e89f7ee682b7db
dikeke <dikeke@qiniu.com>
2025-08-14 17:59:17 +0800

    test

--- src/test_suite.cpp
+++ src/test_suite.cpp
@@ -59,7 +59,7 @@ void test_csp_mode(EuMotorNode& motor) {
         std::cout << "Motor configured for CSP mode. Sending a sine wave trajectory." << std::endl;
         hreal32 start_pos = 0;
         for (int i = 0; i <= 200; ++i) {
-            hreal32 target_pos = start_pos + 15.0f * std::sin(2.0 * M_PI * i / 200.0);
+            hreal32 target_pos = start_pos + 5.0f * std::sin(2.0 * M_PI * i / 200.0);
             motor.sendCspTargetPosition(target_pos, 0, true);
             std::this_thread::sleep_for(std::chrono::milliseconds(50));
         }
