bfef77227221c80138d0917cd6bff2a1f5470203
dikeke <dikeke@qiniu.com>
2025-08-27 10:56:53 +0800

    eu motor

--- include/eu_motor.h
+++ include/eu_motor.h
@@ -88,15 +88,7 @@ public:
 
     static void canRecvCallback(int devIndex, const harmonic_CanMsg* frame);
 
-    bool setPositionGains(huint16 kp, huint16 ki);
-    bool setPositionKp(huint16 kp);
-    bool setPositionKi(huint16 ki);
-    bool setVelocityGains(huint16 kp, huint16 ki);
-    bool setVelocityKp(huint16 kp);
-    bool setVelocityKi(huint16 ki);
-    bool setCurrentGains(huint16 kp, huint16 ki);
-    bool setCurrentKp(huint16 kp);
-    bool setCurrentKi(huint16 ki);
+
 private:
     MotorFeedbackManager() = default;
     ~MotorFeedbackManager();
@@ -339,6 +331,16 @@ public:
     bool startErrorFeedbackTPDO(huint16 pdo_index, huint8 transmit_type = 254, huint16 event_timer_ms = 10);    
 
     int getNodeId();
+
+    bool setPositionGains(huint16 kp, huint16 ki);
+    bool setPositionKp(huint16 kp);
+    bool setPositionKi(huint16 ki);
+    bool setVelocityGains(huint16 kp, huint16 ki);
+    bool setVelocityKp(huint16 kp);
+    bool setVelocityKi(huint16 ki);
+    bool setCurrentGains(huint16 kp, huint16 ki);
+    bool setCurrentKp(huint16 kp);
+    bool setCurrentKi(huint16 ki);
 private:
     huint8 dev_index_;
     huint8 node_id_;
