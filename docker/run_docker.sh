4accc90db0a999a609c1ba9f37a36ef4cb2e17d2
coco.dee <coco.dee@gmail.com>
2025-09-10 08:16:02 +0700

    build

--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -1,5 +1,5 @@
 # 定义你的Docker镜像名称
-IMAGE_NAME="eu_motor_base:latest"
+IMAGE_NAME="eu_motor_base:orin"
 
 # 运行容器，并将当前目录映射到/app
 # -it:  以交互模式运行，并分配一个伪终端 (方便调试)
@@ -7,3 +7,4 @@ IMAGE_NAME="eu_motor_base:latest"
 # -v:   挂载卷，格式是 <host-path>:<container-path>
 # ${PWD}:  这是一个Shell变量，代表“Print Working Directory”，即当前目录
 docker run -it --rm -v "${PWD}:/app" ${IMAGE_NAME} /bin/bash
+
